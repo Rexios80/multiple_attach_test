0d5935b3a73691ffc186ba5b5c6f26301aeb66c8
Rexios <rexios@rexios.dev>
2024-06-23 15:37:48 -0400

    Gutting things

--- lib/multiple_attach_test.dart
+++
@@ -1,8 +0,0 @@
-
-import 'multiple_attach_test_platform_interface.dart';
-
-class MultipleAttachTest {
-  Future<String?> getPlatformVersion() {
-    return MultipleAttachTestPlatform.instance.getPlatformVersion();
-  }
-}
