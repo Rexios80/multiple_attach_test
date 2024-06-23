35bde5f954a43d2f1ecb305a4501893c5b7cb36b
Rexios <rexios@rexios.dev>
2024-06-23 15:48:20 -0400

    ...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,1 +1,7 @@
-void main() {}
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AndroidAlarmManager.initialize();
+}
