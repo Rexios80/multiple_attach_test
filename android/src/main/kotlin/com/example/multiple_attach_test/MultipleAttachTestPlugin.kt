a57da42ded3fb5ed3ebd5bf2e0d24aed40c4cc6c
Rexios <rexios@rexios.dev>
2024-06-23 15:43:35 -0400

    Add `android_alarm_manager_plus`

--- android/src/main/kotlin/com/example/multiple_attach_test/MultipleAttachTestPlugin.kt
+++ android/src/main/kotlin/com/example/multiple_attach_test/MultipleAttachTestPlugin.kt
@@ -1,35 +1,22 @@
 package com.example.multiple_attach_test
 
-import androidx.annotation.NonNull
-
+import io.flutter.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** MultipleAttachTestPlugin */
-class MultipleAttachTestPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "multiple_attach_test")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+class MultipleAttachTestPlugin : FlutterPlugin, MethodCallHandler {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        Log.d("MultipleAttachTestPlugin", "onAttachedToEngine")
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        Log.d("MultipleAttachTestPlugin", "onMethodCall")
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        Log.d("MultipleAttachTestPlugin", "onDetachedFromEngine")
+    }
 }
